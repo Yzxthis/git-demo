587232ddee1902b30a86d392b8648d967373b9b3
yin <wds>
2022-07-31 20:42:46 +0800

    second commit

--- src/main/java/com/gittestdemo/git/GitTest.java
+++ src/main/java/com/gittestdemo/git/GitTest.java
@@ -3,5 +3,6 @@ package com.gittestdemo.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("dwqdq");
+        System.out.println("dwqqwedq");
     }
 }
